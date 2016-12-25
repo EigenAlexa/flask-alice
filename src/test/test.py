68be02b97dc50d0516687cd5ae2d2bd0191b6ff1
philkuz <superpker@gmail.com>
2016-12-25 13:16:43 -0800

    Remove directory dependence for the test

--- src/test/test.py
+++ src/test/test.py
@@ -2,14 +2,16 @@ from __future__ import print_function
 import httplib
 import json
 import argparse
+import os
 
 HOST = "127.0.0.1:5000"
+DIR = os.path.dirname(os.path.realpath(__file__)) + '/'
 # load the request body
-with open('./request.json') as request_file:
+with open(DIR + 'request.json') as request_file:
     request = json.load(request_file)
 
 
-with open('./header.json') as header_file:
+with open(DIR + 'header.json') as header_file:
     header = json.load(header_file)
     # header["Host"] = HOST
 parser = argparse.ArgumentParser(description='Test a phrase with Alexa')
