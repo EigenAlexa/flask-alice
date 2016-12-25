820b9f8da27b6020f75303f0ffc726eedd22e9df
philkuz <superpker@gmail.com>
2016-12-24 20:46:11 -0800

    Fix upload.sh to work with parameters

--- src/upload.sh
+++ src/upload.sh
@@ -1,2 +1,11 @@
-zip -r code.zip src
-aws lambda update-function-code --function-name JamesTestEigen --zip-file fileb://code.zip 
+FORMAT="Command should be formatted as 'upload.sh <dir> <function-name>'"
+ZIPFILE="code.zip"
+if [ $# -ne 2 ]; then
+    echo $FORMAT 
+    exit 1
+elif [ $# -eq 2 ]; then
+    dir=$1
+    funcname=$2
+fi
+zip -r $ZIPFILE $dir
+aws --region us-east-1 lambda update-function-code --function-name $funcname --zip-file fileb://$ZIPFILE
