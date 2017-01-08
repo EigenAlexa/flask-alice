a76aa872b28e7e21973fdfec6afed91a62ea0bf6
philkuz <superpker@gmail.com>
2017-01-07 20:45:33 -0800

    Actually forgot to add the question part of the app

--- src/app.py
+++ src/app.py
@@ -12,12 +12,12 @@ alice = Alice()
 @ask.launch
 def new_session():
     welcome_msg = render_template('hello')
-    return statement(welcome_msg)
+    return question(welcome_msg)
 
 @ask.intent("AllIntent", convert={'All': str})
 def next_round(All):
     response = alice.message(All)
-    return statement(response) # TODO make a handler to detect whether a question has been asked
+    return question(response) # TODO make a handler to detect whether a question has been asked
 
 if __name__ == '__main__':
     app.run(debug=True)
